4fe461f79b4bfbf975a91995d235716db6dbd4ab
Erwan MAS <erwan@mas.nom.fr>
2020-02-12 22:32:36 -0500

    bug_buggy_chunks.sql to verify  buggy chunks bug_create_buggy.sql to create  buggy chunks

---
+++ bug_create_buggy.sql
@@ -0,0 +1,39 @@
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 1000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 1000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 1000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 1000 ) f ;
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 2000 ) f ;
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 4000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 4000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 4000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 4000 ) f ;
+
+
+
+
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 40000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 40000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 40000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 40000 ) f ;
+
+
+
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+
+
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
+
+
+insert into test.bug_consistent_read select * from ( select e.* from test.bug_consistent_read_feed e left join test.bug_consistent_read c  on c.clientid=e.clientid and c.ticketid=e.ticketid where c.clientid is null and c.ticketid is null limit 80000 ) f ;
