25c79d79bc08c526ca833f817266a0c4dc642768
Erwan MAS <erwan@mas.nom.fr>
2020-02-17 15:41:14 -0500

    limit tests to some versions

--- bug_test_in_docker.sh
+++ bug_test_in_docker.sh
@@ -5,11 +5,17 @@ then
         exit 1
 fi
 echo
-PARA_CNT=1
+#
+SOME_MYSQL="mysql/mysql-server:8.0.19-1.1.15=mysql_8_0_19  mysql/mysql-server:8.0.4-1.1.3=mysql_8_0_4 mysql/mysql-server:8.0.1-1.0.0=mysql_8_0_1 mysql/mysql-server:5.7.29-1.1.15=mysql_5_7_29 mysql/mysql-server:5.6.47-1.1.15=mysql_5_6_47 mysql/mysql-server:5.6.36=mysql_5_6_36  mysql/mysql-server:5.6.35=mysql_5_6_35 mysql/mysql-server:5.6.23=mysql_5_6_23 mysql/mysql-server:5.5.62-1.1.10=mysql_5_5_62"
+SOME_MARIA="mariadb:10.4.12=maria_10_4_12 mariadb:10.3.22=maria_10_3_22 mariadb:10.2.31=maria_10_2_31 mariadb:10.1.44=maria_10_1_44 mariadb:10.1.22=maria_10_1_22 mariadb:10.1.11=maria_10_1_11 mariadb:5.5.64=maria_5_5_64"
+SOME_PERCO="percona:ps-8.0.18-9=perco_8_0_18 percona/percona-server:8.0.13=perco_8_0_13 percona:ps-5.7.29=perco_5_7_29 percona:ps-5.6.47=perco_5_6_47 percona:5.6.36=perco_5_6_36 percona:5.6.35=perco_5_6_35 percona:5.5.61=perco_5_5_61"
+#
+BUG_KO="mysql/mysql-server:5.6.35=mysql_5_6_35 mysql/mysql-server:5.7.17=mysql_5_7_17 percona:5.6.35=perco_5_6_35 percona:5.7.17=perco_5_7_17"
+BUG_OK="mysql/mysql-server:5.6.36=mysql_5_6_36 mysql/mysql-server:5.7.18=mysql_5_7_18 percona:5.6.36=perco_5_6_36 percona:5.7.18=perco_5_7_18 mysql/mysql-server:5.7.29-1.1.15=mysql_5_7_29 percona:ps-5.7.29=perco_5_7_29"
+#
+PARA_CNT=4
 C=0
-for V in mysql/mysql-server:8.0.19-1.1.15=mysql_8_0_19  mysql/mysql-server:8.0.4-1.1.3=mysql_8_0_4  mysql/mysql-server:5.7.29-1.1.15=mysql_5_7_29 mysql/mysql-server:5.6.47-1.1.15=mysql_5_6_47 mysql/mysql-server:5.5.62-1.1.10=mysql_5_5_62 \
-       	 mariadb:10.4.12=maria_10_4_12 mariadb:10.3.22=maria_10_3_22 mariadb:10.2.31=maria_10_2_31 mariadb:10.1.44=maria_10_1_44 mariadb:10.1.22=maria_10_1_22 mariadb:10.1.11=maria_10_1_11 mariadb:5.5.64=maria_5_5_64 \
-         percona:ps-8.0.18-9=perco_8_0_18 percona/percona-server:8.0.13=perco_8_0_13 percona:ps-5.7.29=perco_5_7_29 percona:ps-5.6.47=perco_5_6_47 percona:5.5.61=perco_5_5_61
+for V in $BUG_KO $BUG_OK
 do
         C=$(( $C + 1 ))
 	(
@@ -19,9 +25,11 @@ do
                 echo $NAM
 
                 docker run --name ${NAM} -e MYSQL_ROOT_PASSWORD=test1234 -d ${IMG} || exit 1
-                while [ $( docker exec -i $NAM  mysqladmin -h localhost -u root -ptest1234 ping 2>&1 | grep -c 'mysqld is alive' ) -eq 0 ]
+		T=30
+                while [ $( docker exec -i $NAM  mysqladmin -h localhost -u root -ptest1234 ping 2>&1 | grep -c 'mysqld is alive' ) -eq 0 -a $T -gt 0 ]
                 do
-                        sleep 2
+                    sleep 2
+		    T=$(( $T - 1 ))
                 done
                 docker exec -i $NAM  sh -c '/usr/bin/mysql  -u root -ptest1234  -h localhost -e "create database test ; " '
 
@@ -45,8 +53,9 @@ do
            wait
        fi
 done
+wait
 echo version that are buggy
-grep -l -e '_20000_.*: [1]' -e '_10000_.*: [2-9]' -e '_15000_.*: 1[0-4]' -e '_5000_.*: [0-4]'  /tmp/output_*log
+grep -l -e '_20000_.*: [1]' -e '_10000_.*: [2-9]' -e '_15000_.*: 1[0-4]' -e '_5000_.*: [0-4]'  /tmp/output_*log  | sort -t_ -k3,5
 
 
 
